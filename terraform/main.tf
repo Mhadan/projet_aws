8c232ffa4c6ca1019da353827021aa6a5e49e523
Mhadan <mohamedmehdi.bengharbi@iteam-univ.tn>
2026-05-21 13:25:45 +0200

    fix: clean main.tf with stable SG

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,33 @@
+erraform {
+  backend "s3" {
+    bucket = "terraform-state-projet-aws-1779358043"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+ 
 provider "aws" {
   region = "us-east-1"
 }
-
-# =====================
-# DEFAULT VPC
-# =====================
+ 
 data "aws_vpc" "default" {
   default = true
 }
-
+ 
 data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
 }
-
-# =====================
-# SECURITY GROUP
-# =====================
+ 
 resource "aws_security_group" "web_sg" {
-  name        = "web-sg-${formatdate("YYYYMMDDhhmmss", timestamp())}"
-  vpc_id      = data.aws_vpc.default.id
+  name   = "web-sg"
+  vpc_id = data.aws_vpc.default.id
+ 
+  lifecycle {
+    create_before_destroy = true
+  }
  
   ingress {
     from_port   = 22
@@ -48,36 +54,22 @@ resource "aws_security_group" "web_sg" {
     Name = "web-sg"
   }
 }
-
-# =====================
-# EC2 INSTANCES
-# =====================
+ 
 resource "aws_instance" "web" {
   count         = 2
-  ami           = "ami-02b2c1b57c5105166" # Amazon Linux 2 (us-east-1)
+  ami           = "ami-02b2c1b57c5105166"
   instance_type = "t2.micro"
-   key_name = "vockey"
-
-  subnet_id              = data.aws_subnets.default.ids[0]
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-
+  key_name      = "vockey"
+ 
+  subnet_id                   = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
-
+ 
   tags = {
     Name = "ecommerce-server"
   }
 }
-
-# =====================
-# OUTPUT
-# =====================
+ 
 output "instance_public_ips" {
   value = aws_instance.web[*].public_ip
-}
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-projet-aws-1779358043"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
 }
